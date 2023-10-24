210b7c02a983e400e3be20b4702cb7447f820a52
AlvaroBuleo <algobul@gmail.com>
2023-10-24 21:17:40 +0200

    Implementada función QuickSort

--- DyV/DyV.h
+++ DyV/DyV.h
@@ -1,5 +1,15 @@
 #include <vector>
 #include <iostream>
+
+template <typename T>
+
+void printvec(std::vector<T>& V,int tam){
+	for(int i = 0;i < tam;i++){
+		std::cout << V[i] << " ";
+	}	
+	std::cout << std::endl;
+}
+
 template <typename T>
 
 int BusquedaBinaria(T X,std::vector<T>& V, int ini, int fin){
@@ -33,3 +43,49 @@ int BusquedaBinaria_INV(T X,std::vector<T>& V,int ini,int fin){
         else
                 return BusquedaBinaria_INV(X, V, medio + 1, fin);
 }
+
+template <typename T>
+
+int Partition(std::vector<T>& V,int ini,int fin){
+	T x = V[fin];
+	T aux;
+	int i = ini;
+	for(int j = ini;j < fin;j++){
+		if(V[j] <= x){
+#ifdef DEBUG
+			std::cout << "Swap " << V[i] << " " << V[j] << std:: endl;
+#endif
+			aux = V[i];
+			V[i] = V[j];
+			V[j] = aux;
+#ifdef DEBUG
+			printvec(V,5);
+#endif
+			i++;
+		}
+	}	
+#ifdef DEBUG
+	std::cout << "Swap " << V[i] << " " << V[fin] << std:: endl;
+#endif
+	aux = V[i];
+	V[i] = V[fin];
+	V[fin] = aux;
+#ifdef DEBUG
+	printvec(V,5);
+#endif
+	return i;
+}	
+
+template <typename T>
+
+void QuickSort(std::vector<T>& V,int ini,int fin){
+	if(ini < fin){
+		int pivot = Partition(V,ini,fin);
+		QuickSort(V,ini,pivot-1);
+		QuickSort(V,pivot + 1,fin);
+	}
+}
+
+
+
+
--- DyV/test_DyV.cpp
+++ DyV/test_DyV.cpp
@@ -2,12 +2,31 @@
 #include <vector>
 
 int main(){
+	
 	std::vector<int> a{1,3,25};
 	std::vector<int> inv{21,6,2};
 	std::vector<double> b{1.1,3.7,6.2};
+	
+	std::vector<int> intdesord{9,7,6,1,2};
+	std::vector<char> chardesord{'c','d','b','a'};
+	
 	std::cout << "Busqueda Binaria Int: " << BusquedaBinaria(25,a,0,2)  << "\n";
 	std::cout << "Busqueda Binaria Int Inverso: " << BusquedaBinaria_INV(2,inv,0,2)  << "\n";	
 	std::cout << "Busqueda Binaria Double: " << BusquedaBinaria(6.2,b,0,2)  << "\n";
+	
+	std::cout << "Vector de int desordenados: ";
+        printvec(intdesord,5);
 
+	QuickSort(intdesord,0,4);
+	std::cout << "Vector de int ordenados: ";
+	printvec(intdesord,5);
+	
+	std::cout << "Vector de char desordenados: ";
+        printvec(chardesord,4);
+
+	QuickSort(chardesord,0,3);
+	std::cout << "Vector de char ordenados: ";
+	printvec(chardesord,4);
+	
 	return 0;
 }
