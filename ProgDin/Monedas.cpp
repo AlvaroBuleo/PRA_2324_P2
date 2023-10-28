b3d136ea7fe764e8e8244d867974e6a170cdd6e2
AlvaroBuleo <algobul@gmail.com>
2023-10-28 20:13:16 +0200

    Programa monedas devuelve cantidad de cada

--- ProgDin/Monedas.cpp
+++ ProgDin/Monedas.cpp
@@ -1,29 +1,7 @@
 #include <iostream>
+#include <stdlib.h>
 #define N 3
-/*
-void printTabla(int v[][9],int fil,int col){
-	for(int i = 0;i < fil;i++){
-		for(int j = 0;j < col;j++){
-			std::cout << v[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-}
 
-int cambio(int* v,int n,int size){
-	int tab[size][n+1];
-	for(int i = 0;i < size;i++){	//filas
-		for(int j = 0;j <= n;j++){ //columnas
-			if(j == 0)
-				tab[i][j] = 0;
-			else
-				tab[i][j] = -1;
-		}	
-	}
-	printTabla(tab,size,n+1);
-	return 0;
-}
-*/
 
 int min(int x, int y){
 	if(x < y)
@@ -32,7 +10,7 @@ int min(int x, int y){
 		return y;
 }
 
-int cambio(int* v,int P){
+int cambio(int* v,int P,int* vector){
 	int tab[N][P+1];
 	for(int i = 0;i < N;i++){
 		for(int j = 0;j <= P;j++){
@@ -40,15 +18,16 @@ int cambio(int* v,int P){
 				tab[i][j] = -1;
 		}
 	}
-	tab[0][0] = 0;
-        for(int j = 1;j <= P + 1;j++){
-        	if(j >= v[0] && tab[0][j - v[0]] != -1){
-			tab[0][j] = 1 + tab[0][j - v[0]];
-		}
+	tab[0][0] = 0;						//Primera 
+        for(int j = 1;j <= P + 1;j++){				//Fila
+        	if(j >= v[0] && tab[0][j - v[0]] != -1){	//De
+			tab[0][j] = 1 + tab[0][j - v[0]];	//La
+		}						//Tabla
 	}
-	for(int i = 1;i < N;i++){
-		tab[i][0] = 0;
-		for(int j = 1;j <= P;j++){
+	
+	for(int i = 1;i < N;i++){							//Resto
+		tab[i][0] = 0;								//De
+		for(int j = 1;j <= P;j++){						//Filas
 			if(j < v[i] || tab[i][j - v[i]] == -1)
 				tab[i][j] = tab[i - 1][j];
 			else if(tab[i - 1][j] != -1)
@@ -57,13 +36,34 @@ int cambio(int* v,int P){
 				tab[i][j] = 1 + tab[i][j - v[i]];
 		}
 	}
+	
+	int i = N - 1;
+	int j = P;
+	
+	while(j > 0){
+		if(tab[i][j] == tab[i-1][j]){
+			i--;
+		}
+		else{
+			j -= v[i];
+			vector[i]++;
+		}	
+	}
+	
 	return tab[N-1][P];
 }
 
-int main(){
-	int v[3] = {1,4,6};
-	std::cout << "Monedas minimas: " <<  cambio(v,11) << std::endl;
-
+int main(int argc,char** argv){
+	int v[N] = {1,4,6};
+	int vector[N];
+	int cant = atoi(argv[1]);
+	
+	std::cout << "Monedas minimas: " <<  cambio(v,cant,vector) << std::endl;
+	
+	for(int i = 0;i < N;i++){
+		std::cout << vector[i] << " monedas de " << v[i] << std::endl;
+	}
+	
 	return 0;
 }
 
